0ef182d2beb5931cce1f4e97e98be091b1cd881c
Tigran Simonyan <tigsim97@gmail.com>
2025-05-02 19:05:59 +0400

    Updated to sxframework

---
+++ ScyllaLivelinessSDK.xcframework/ios-arm64_x86_64-simulator/ScyllaLivelinessSDKFramework.framework/Headers/ScyllaLivelinessSDKFramework.h
@@ -0,0 +1,18 @@
+//
+//  ScyllaLivelinessSDKFramework.h
+//  ScyllaLivelinessSDKFramework
+//
+//  Created by Tigran Simonyan on 29.04.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScyllaLivelinessSDKFramework.
+FOUNDATION_EXPORT double ScyllaLivelinessSDKFrameworkVersionNumber;
+
+//! Project version string for ScyllaLivelinessSDKFramework.
+FOUNDATION_EXPORT const unsigned char ScyllaLivelinessSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScyllaLivelinessSDKFramework/PublicHeader.h>
+
+
